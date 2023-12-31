949dd483445f349322a19faf0545393ebfaf0cb1
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-12-31 13:40:26 +0100

    Features section complete

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { FEATURES } from "@/constants";
+import Image from "next/image";
+import React from "react";
 
 const Features = () => {
-  return (
-    <div>Features</div>
-  )
-}
+	return (
+		<section className="border-2 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+			<div className="max-container relative w-full flex items-center justify-end ">
+				<div className="flex flex-1 lg:min-h-[900px]">
+					{/* Left */}
+					<Image
+						src="/phone.png"
+						alt="phone"
+						width={440}
+						height={1000}
+						className="feature-phone"
+					/>
+				</div>
 
-export default Features
+				<div className="z-20 flex w-full flex-col lg:w-[60%]">
+					<div className="relative">
+						<Image
+							src="/camp.svg"
+							alt="camp"
+							width={50}
+							height={50}
+							className="absolute left-[-5px] top-[-28px]"
+						/>
+						<h2 className="bold-40 lg:bold-64">Our Features</h2>
+					</div>
+					<ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mb-20 lg:gap-20 ">
+						{FEATURES.map((feature) => (
+							<FeatureItem
+								title={feature.title}
+								key={feature.title}
+								icon={feature.icon}
+								description={feature.description}
+								variant={feature.variant}
+							/>
+						))}
+					</ul>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+type FeatureItemProps = {
+	title: string;
+	icon: string;
+	description: string;
+	variant: string;
+};
+const FeatureItem = ({ title, icon, description }: FeatureItemProps) => {
+	return (
+		<li className="flex w-full flex-1 flex-col items-start">
+			<div className="rounded-full p-4 lg:p-7 bg-green-50">
+				<Image src={icon} alt="map" width={28} height={28} />
+			</div>
+			<h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+			<p className="regular-16 mt-5 bg-white/80">{description}</p>
+		</li>
+	);
+};
+
+export default Features;
